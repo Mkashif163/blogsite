8fd33f607be39a5cdc0951b123fceeb09688ceaa
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2023-08-10 12:17:41 +0500

    upadated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,59 +16,64 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://kashifadmin:itsmekashif@cluster0.qdnyewt.mongodb.net/blogDB");
+async function connectDB() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("Connected to DB");
+}
+
+connectDB();
 
 
 const postSchema = {
 
   title: String,
-  
+
   content: String
-  
-  };
+
+};
 
 
-  const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 
 
-app.get("/", function(req, res){
-  Post.find({}).then(function(posts){
+app.get("/", async function (req, res) {
+  await Post.find({}).then(function (posts) {
 
     res.render("home", {
-    
-    startingContent: homeStartingContent,
-    
-    posts: posts
-    
+
+      startingContent: homeStartingContent,
+
+      posts: posts
+
     });
-    
-    })
+
+  })
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  const post = new Post ({
+app.post("/compose", function (req, res) {
+  const post = new Post({
 
     title: req.body.postTitle,
-    
+
     content: req.body.postBody
-    
-    });
+
+  });
 
   post.save();
 
@@ -76,21 +81,21 @@ app.post("/compose", function(req, res){
 
 });
 
-app.get("/posts/:postId", function(req, res){
+app.get("/posts/:postId",async function (req, res) {
   const requestedPostId = req.params.postId;
 
-  Post.findOne({_id:requestedPostId}).then(function (post) {
+  await Post.findOne({ _id: requestedPostId }).then(function (post) {
     res.render("post", {
 
       title: post.title,
-      
+
       content: post.content
-      
-      });
+
     });
+  });
 
 });
 
-app.listen(port, function() {
-  console.log("Server started"+port);
+app.listen(port, function () {
+  console.log("Server started" + port);
 });
